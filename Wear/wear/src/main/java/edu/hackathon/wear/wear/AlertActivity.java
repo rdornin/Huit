657074b629a2fb39a7b7b917f955f61d13ffb596
rdornin <rebecca.dornin@gmail.com>
2015-01-10 13:38:47 -0500

    adding in first basic alarm

---
+++ Wear/wear/src/main/java/edu/hackathon/wear/wear/AlertActivity.java
@@ -0,0 +1,51 @@
+package edu.hackathon.wear.wear;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class AlertActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                TextView mTextView = (TextView) stub.findViewById(R.id.text);
+                //TextView text = (TextView) findViewById(R.id.this_is_the_id_of_textview);
+                mTextView.setText("alert");
+            }
+
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_alert, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- Wear/wear/src/main/java/edu/hackathon/wear/wear/MainActivity.java
+++ Wear/wear/src/main/java/edu/hackathon/wear/wear/MainActivity.java
@@ -1,13 +1,23 @@
 package edu.hackathon.wear.wear;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,30 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                //TextView text = (TextView) findViewById(R.id.this_is_the_id_of_textview);
+                mTextView.setText("test");
+                /*context = getActivity();
+
+                alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+                Intent intent = new Intent(context,MainActivity.class);
+                alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+                alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        SystemClock.elapsedRealtime() +
+                                10 * 1000, alarmIntent);*/
+                Intent i = new Intent(getApplicationContext(), AlertActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),3333,i,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+
+                //getting current time and add 5 seconds in it
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+                //registering our pending intent with alarmmanager
+                AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(), pi);
             }
         });
+
+
     }
 }
